4fcb4628cd82c00d5f3789772e356445ae652ee7
Matthew Hill <N00170079@iadt.ie>
2019-10-25 14:53:48 +0100

    working on mos vid 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Welcome</div>
+                <div class="card-header">About Us</div>
 
                 <div class="card-body">
-                  Welcome to Gradr!! Learn more <a href="{{route('about')}}">About us</a>
+                This is the about page! <a href="#"></a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,22 @@
 */
 
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'PageController@welcome')->name('welcome');
+Route::get('/about', 'PageController@about')->name('about');
+
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin/home', 'Admin\HomeController@index')->name('admin.home');
 Route::get('/secretary/home', 'Secretary\HomeController@index')->name('secretary.home');
 Route::get('/user/home', 'User\HomeController@index')->name('user.home');
+
+Route::get('/admin/questions', 'Admin\QuestionController@index')->name('admin.questions.index');
+Route::get('/admin/questions/create', 'Admin\QuestionController@create')->name('admin.questions.create');
+Route::get('/admin/questions/{id}', 'Admin\QuestionController@show')->name('admin.questions.show');
+Route::post('/admin/questions/store', 'Admin\QuestionController@store')->name('admin.questions.store');
+Route::get('/admin/questions{id}/edit', 'Admin\QuestionController@edit')->name('admin.questions.edit');
+Route::put('/admin/questions/{id}', 'Admin\QuestionController@update')->name('admin.questions.update');
+Route::delete('/admin/questions/{id}', 'Admin\QuestionController@destroy')->name('admin.questions.destroy');
+
+Route::get('/user/questions', 'User\QuestionController@index')->name('user.questions.index');
+Route::get('/user/questions/{id}', 'User\QuestionController@index')->name('user.questions.show');
